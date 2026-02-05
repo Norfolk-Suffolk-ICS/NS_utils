c1fd96e4e224773f8b7e7c53f52ec7d321fe44c8
Ibrahim Khan <Ibrahim.Khan@suffolk.gov.uk>
2026-02-05 13:56:42 +0000

    setup changes

--- setup.py
+++ setup.py
@@ -8,19 +8,10 @@ setup(
     author="Ibrahim Khan",
     license="Apache-2.0",
     license_files=["LICENSE"],  # correct field name
-    install_requires=[
-        "pandas>=2.3.0",
-        "numpy>=2.0.0",
-        "sqlalchemy>=2.0.0",
-        "snowflake-connector-python>=4.2.0",
-        "toml>=0.10.2",
-        "pyarrow>=9.0.0"
-        # Add other dependencies from your requirements.txt as needed
-    ],
+    install_requires=[],
     include_package_data=True,  # include non-code files via MANIFEST.in
     package_data={
         "snee_styles": ["styles/SNEE.mplstyle", "styles/SNEE_plotly.json"],
         # py_utils usually does not need package_data unless you have non-code files
     },
-    python_requires=">=3.8",  # optional: enforce minimum Python version
 )
